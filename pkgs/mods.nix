d8da77252e073fbcb9cb185cc314991cd25e7dcb
Marie Bonnelle <marie.bonnelle@proton.me>
2026-08-13 18:35:56 -0700

    Add Multiplayer mod

--- pkgs/mods.nix
+++ pkgs/mods.nix
@@ -10,6 +10,14 @@
     hash = "sha256-VN/n4Hotvlejs/jn+EOFJ6r7o7cSWtHJBEJdBly5oRA=";
   };
 
+  multiplayer = fetchFromGitHub {
+    name = "multiplayer";
+    owner = "Balatro-Multiplayer";
+    repo = "BalatroMultiplayer";
+    tag = "v0.5.5";
+    hash = "sha256-uYUJbIjQaztSqEQLybTTt3xi+aRg/cI5+BqNRU+ZdN0=";
+  };
+
   smods = fetchFromGitHub {
     name = "smods";
     owner = "Steamodded";
