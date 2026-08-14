87b1bbe09e1b5b07d3662821cd7a79714467f6d8
Marie Bonnelle <marie.bonnelle@proton.me>
2026-08-13 18:26:39 -0700

    Code style

--- pkgs/mods.nix
+++ pkgs/mods.nix
@@ -4,17 +4,17 @@
 }:
 
 {
+  balatreat = fetchzip {
+    name = "balatreat";
+    url = "https://nomnomnami.com/games/treat/downloads/BALATREAT(sm-template).zip";
+    hash = "sha256-VN/n4Hotvlejs/jn+EOFJ6r7o7cSWtHJBEJdBly5oRA=";
+  };
+
   smods = fetchFromGitHub {
     name = "smods";
     owner = "Steamodded";
     repo = "smods";
     tag = "1.0.0-beta-1814a";
     hash = "sha256-5chUzZSfUDUqtlMzSdSa1fZRHOPvRIdwHnKK83f4ecs=";
-  };
-
-  balatreat = fetchzip {
-    name = "balatreat";
-    url = "https://nomnomnami.com/games/treat/downloads/BALATREAT(sm-template).zip";
-    hash = "sha256-VN/n4Hotvlejs/jn+EOFJ6r7o7cSWtHJBEJdBly5oRA=";
   };
 }
